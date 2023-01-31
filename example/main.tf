81afa08a7fd993c3818b1b875677fbf389289102
NoeSamaille <noe.samaille@protonmail.com>
2023-01-31 20:53:55 +0100

    Updates `example/` variablesx
    Signed-off-by: NoeSamaille <noe.samaille@protonmail.com>

--- example/main.tf
+++ example/main.tf
@@ -8,13 +8,22 @@ variable "my_public_ip_cidr" {}
 variable "cluster_name" {}
 variable "os_image_id" {}
 variable "certmanager_email_address" {}
+variable "region" {}
 variable "k3s_server_pool_size" {
   default = 1
 }
 variable "k3s_worker_pool_size" {
   default = 2
 }
-variable "region" {}
+variable "k3s_extra_worker_node" {
+  default = true
+}
+variable "expose_kubeapi" {
+  default = false
+}
+variable "environment" {
+  default = "staging"
+}
 
 module "k3s_cluster" {
   # k3s_version               = "v1.23.8+k3s2" # Fix kubectl exec failure
@@ -25,11 +34,13 @@ module "k3s_cluster" {
   compartment_ocid          = var.compartment_ocid
   my_public_ip_cidr         = var.my_public_ip_cidr
   cluster_name              = var.cluster_name
-  environment               = "staging"
+  environment               = var.environment
   os_image_id               = var.os_image_id
   certmanager_email_address = var.certmanager_email_address
   k3s_server_pool_size      = var.k3s_server_pool_size
   k3s_worker_pool_size      = var.k3s_worker_pool_size
+  k3s_extra_worker_node     = var.k3s_extra_worker_node
+  expose_kubeapi            = var.expose_kubeapi
   ingress_controller        = "nginx"
   source                    = "../"
 }
