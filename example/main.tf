3e6eee575b56e42489621a84a84e0cd194a87f0b
lefteris <lefteris@alexakis.org>
2023-09-13 05:51:30 +0200

    change to larger workers

--- example/main.tf
+++ example/main.tf
@@ -3,31 +3,50 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
-variable "availability_domain" {}
+variable "availability_domain" {
+  default = "wYgE:EU-FRANKFURT-1-AD-1"
+}
+variable "public_key_path" {
+  default     = "~/.ssh/id_rsa.pub"
+}
+
 variable "my_public_ip_cidr" {}
-variable "cluster_name" {}
-variable "os_image_id" {}
-variable "certmanager_email_address" {}
-variable "region" {}
+variable "cluster_name" {
+  default = "test-cluster"
+}
+variable "os_image_id" {
+  default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaeusqwc4fgp4c5ienodxnlvrkimp4rp4a6snpnkpudznmdlxt3wpq"
+}
+variable "certmanager_email_address" {
+  default = "changeme@example.com"
+}
+variable "region" {
+  default = "eu-frankfurt-1"
+}
 variable "k3s_server_pool_size" {
   default = 1
 }
 variable "k3s_worker_pool_size" {
-  default = 2
+  default = 0
 }
 variable "k3s_extra_worker_node" {
   default = true
 }
 variable "expose_kubeapi" {
-  default = false
+  default = true
 }
 variable "environment" {
-  default = "staging"
+  default = "prod"
 }
 
 module "k3s_cluster" {
   # k3s_version               = "v1.23.8+k3s2" # Fix kubectl exec failure
   # k3s_version               = "v1.24.4+k3s1" # Kubernetes version compatible with longhorn
+  #k3s_version               = "v1.28.1+k3s1"
+  #k3s_version = "v1.25.13+k3s1"
+  k3s_version = "v1.26.7+k3s1"
+  #k3s_version = "v1.25.13+k3s1"
+
   region                    = var.region
   availability_domain       = var.availability_domain
   tenancy_ocid              = var.tenancy_ocid
